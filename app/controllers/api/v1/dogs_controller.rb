9c84ab7b3749a529171013ee4c93961bf6957946
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-11 12:23:26 -0300

    13° commit - tdd controller: Consegue listar um dog especifico e retornar status 200? GREEN

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::DogsController < ApplicationController
 
-  #before_action :set_dog, only: %i[] #show update destroy
+  before_action :set_dog, only: %i[show] #show update destroy
 
   def index
     @dogs = Dog.all 
     render json: @dogs
+  end
+
+  def show
+    render json: @dog
   end
 
 private
