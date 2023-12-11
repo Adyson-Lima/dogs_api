d4515775f2d383fb3f951decb904ff67ae1c058a
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-11 12:27:50 -0300

    14° commit - tdd controller: Consegue criar um dog e retornar status 201? RED

--- spec/controllers/dogs_controller_v1_spec.rb
+++ spec/controllers/dogs_controller_v1_spec.rb
@@ -20,4 +20,12 @@ RSpec.describe Api::V1::DogsController, type: :controller do
     end
   end
 
+  describe 'POST /api/v1/dogs' do
+    it 'Consegue criar um dog e retornar status 201?' do
+      post :create, params: {dog: {breed: 'buldog', size: 'medio'}, format: :json}
+      expect(response.body).to include_json(breed: 'buldog')
+      expect(response).to have_http_status(201)
+    end
+  end
+
 end
